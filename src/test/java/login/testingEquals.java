0f87336f6f6e41d9161a2980c302af05545f54d3
lsalazar1 <lmsalazar78011@gmail.com>
2020-02-20 01:51:54 -0600

    First Commit

---
+++ src/test/java/login/testingEquals.java
@@ -0,0 +1,15 @@
+package login;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class testingEquals {
+	
+	@Test
+	void equalsFour() {
+		int x = 2 , y = 3;
+		
+		Assert.assertEquals(x+y, 4);
+	}
+	
+}
